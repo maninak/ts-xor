96f45e72d84623330e6e024bb6b9a03472462158
Kostis Maninakis <maninak@protonmail.com>
2019-02-25 00:36:55 +0100

    docs: add descriptive jsdoc annotations

--- src/types/Xor.type.ts
+++ src/types/Xor.type.ts
@@ -1,7 +1,22 @@
-type Without<T, U> = { [P in Exclude<keyof T, keyof U>]?: never }
+type Without<T, U> = {
+  [P in Exclude<keyof T, keyof U>]
+  ?
+  : never
+}
 
 
 /**
- * TODO add jsdoc
+ * Returns a type composed with T and U, restricting usage of either
+ * exclusively to members of T or exclusively to members of U.
+ *
+ * No unique members of T can be used simultaneusly with any
+ * unique members of U.
+ *
+ * Example:
+ * `const myVar: XOR<T, U>`
+ *
+ * More: https://github.com/maninak/ts-xor/tree/master#description
  */
-export type XOR<T, U> = (T | U) extends object ? (Without<T, U> & U) | (Without<U, T> & T) : T | U
+export type XOR<T, U> = (T | U) extends object
+  ? (Without<T, U> & U)
+  | (Without<U, T> & T) : T | U
