c0f19017735c6e48c68096d30e48d7ae2036ef4f
Kostis Maninakis <maninak@protonmail.com>
2019-03-02 16:41:57 +0100

    chore(test): print specific tests that failed

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 
+printFileFailedTest () {
+  printf "\e[38;5;196m\e[48;5;0mTest \e[1;38m$1\e[0m\e[38;5;196m\e[48;5;0m failed!\n\e[0m"
+}
+
+TESTS_FAILED=false
+
 # all these tests must pass compilation
-tsc --noEmit $(ls test/should-pass/*.ts) > /dev/null
-if [ $? -ne 0 ]; then
-  printf '\e[38;5;196m\e[48;5;0mA test failed!\n\e[0m'
-  exit 1
-fi
+for FILE in $(ls test/should-pass/*.ts)
+do
+  tsc --noEmit $FILE > /dev/null
+  if [ $? -ne 0 ]; then
+    TESTS_FAILED=true
+    printFileFailedTest $FILE
+  fi
+done
 
 # each of these tests must fail compilation
 for FILE in $(ls test/should-fail/*.ts)
 do
   tsc --noEmit $FILE > /dev/null
   if [ $? -eq 0 ]; then
-    printf '\e[38;5;196m\e[48;5;0mA test failed!\n\e[0m'
-    exit 1
+    TESTS_FAILED=true
+    printFileFailedTest $FILE
   fi
 done
 
-printf '\e[0;32m\e[48;5;0mAll tests passed.\n\e[0m'
-exit 0
+if $TESTS_FAILED; then
+  exit 1
+else
+  printf "\e[0;32m\e[48;5;0mAll tests passed.\n\e[0m"
+  exit 0
+fi
